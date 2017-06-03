e88b8f82f543cb3e3691e2df15e99a6397930100
Ádám Horváth <horada1991@gmail.com>
2017-06-03 20:17:26 +0200

    UserHandler window
    window for registration and login

---
+++ HomeAccountant/UserHandler.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HomeAccountant
+{
+    /// <summary>
+    /// Interaction logic for UserHandler.xaml
+    /// </summary>
+    public partial class UserHandler : Window
+    {
+        public UserHandler()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Imelement registration here
+
+            throw new NotImplementedException();
+        }
+    }
+}
